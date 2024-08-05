7f90afe7679f9fe2d26e51e782e76f78a1144446
CatalinChira <catalin_chira05@yahoo.com>
2024-08-05 22:30:43 +0300

    added users to the db

--- src/main/java/com/example/springsec1/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsec1/config/ProjectSecurityConfig.java
@@ -1,16 +1,11 @@
 package com.example.springsec1.config;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
@@ -22,19 +17,19 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         //http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","/myBalance","/myCards","/myLoans").authenticated()
-                .requestMatchers("/notices","/contact","/error").permitAll());
+        http.csrf(csrfConfig->csrfConfig.disable())
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","/myBalance","/myCards","/myLoans").authenticated()
+                .requestMatchers("/notices","/contact","/error","/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user=User.withUsername("user").password("{noop}CatalinChira@12345").roles("read").build();
-        UserDetails admin=User.withUsername("admin").password("{bcrypt}$2a$12$yxdJOdtNR05bdDMT1WmbfOP8e1gUKzyFhjcOktJT/pGssGWBh4qU2").roles("admin").build();
-        return new InMemoryUserDetailsManager(user,admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
---
+++ src/main/java/com/example/springsec1/model/Customer.java
@@ -0,0 +1,18 @@
+package com.example.springsec1.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="customer")
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String pwd;
+    @Column(name="role")
+    private String role;
+}
