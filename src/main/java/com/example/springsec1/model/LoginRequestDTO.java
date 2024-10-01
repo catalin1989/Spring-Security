6df15cc713b372f5d1351e83178f8b6fbb28b060
CatalinChira <catalin_chira05@yahoo.com>
2024-10-01 15:18:30 +0300

    Added JWT authentification

---
+++ src/main/java/com/example/springsec1/model/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.springsec1.model;
+
+public record LoginRequestDTO(String username, String password) {
+}
---
+++ src/main/java/com/example/springsec1/model/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.springsec1.model;
+
+public record LoginResponseDTO(String status,String jwtToken) {
+}
